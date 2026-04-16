a198e9e890ecbb9cc41fface7ca4d00c1b8b8292
Edwin Cardenas <eacardenase@gmail.com>
2026-04-16 15:00:34 -0500

    feat: style up the UI

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,26 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
 
     var body: some View {
-        VStack {
-            Stepper(
-                "\(sleepAmount.formatted()) hours",
-                value: $sleepAmount,
-                in: 4...12,
-                step: 0.25
-            )
+        NavigationStack {
+            VStack(spacing: 24) {
+                VStack {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
 
-            DatePicker(
-                "Please enter a date",
-                selection: $wakeUp,
-                in: Date.now...,
-            )
-            .labelsHidden()
+                    DatePicker(
+                        "Please enter a time",
+                        selection: $wakeUp,
+                        displayedComponents: .hourAndMinute
+                    )
+                    .labelsHidden()
+                }
+
+                VStack {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+
+                    Stepper(
+                        "\(sleepAmount.formatted()) hours",
+                        value: $sleepAmount,
+                        in: 4...12,
+                        step: 0.25
+                    )
+                }
+
+                VStack {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+
+                    Stepper(
+                        "\(coffeeAmount) cup(s)",
+                        value: $coffeeAmount,
+                        in: 1...20
+                    )
+                }
+            }
+            .padding()
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
         }
-        .padding()
+    }
+
+    func calculateBedtime() {
+        print(#function)
     }
 }
 
